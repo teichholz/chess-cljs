1f79215580d4e5b42dd4414ec4e6079d6ce67e17
Tim Eichholz <tim.eichholz98@web.de>
2019-10-08 20:42:40 +0200

    Anfang der Figurmovements

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -21,8 +21,8 @@
                  :justify-content "center"
                  :align-items "center"})
 (def css-figure {:display "block"
-                 :height (- (:cell css-state) (:figure-gap css-state))
-                 :width (- (:cell css-state) (:figure-gap css-state))})
+                 :width (str (- (:cell css-state) (:figure-gap css-state)) "px")
+                 :height (str (- (:cell css-state) (:figure-gap css-state)) "px")})
 
 
 (defn dispatch-img [fig team]
@@ -61,25 +61,26 @@
 (defn create-figure [fig team]
   (assoc
     (cond
-      (= :pawn fig) (Pawn. team)
+      (= :pawn fig) (Pawn. team nil)
       (= :noble fig) (Noble. team)
       (= :bishop fig) (Bishop. team)
       (= :queen fig) (Queen. team)
       (= :king fig) (King. team)
-      (= :castle fig) (Castle. team))
+      (= :rastle fig) (Castle. team))
     :img (dispatch-img fig team)))
-
-(create-figure :pawn :light)
 
 
 (defn init-board []
   "Gibt ein 8x8 Spielbrett in Form von Vektoren (Arraylists) zurück"
-  (vec (repeat 8 (vec (range 1 9)))))
+  (let [pawn-light (repeat 8 (create-figure :pawn :light))
+        pawn-dark (repeat 8 (create-figure :pawn :dark))
+        backline-light [(create-figure :rastle :light) (create-figure :noble :light) (create-figure :bishop :light) (create-figure :queen :light)
+                        (create-figure :king :light) (create-figure :noble :light) (create-figure :bishop :light) (create-figure :rastle :light)]
+        backline-dark [(create-figure :rastle :dark) (create-figure :noble :dark) (create-figure :bishop :dark) (create-figure :queen :dark)
+                        (create-figure :king :dark) (create-figure :noble :dark) (create-figure :bishop :dark) (create-figure :rastle :dark)]]
+    [backline-light (vec pawn-light) (vec (repeat 8 nil)) (vec (repeat 8 nil)) (vec (repeat 8 nil)) (vec (repeat 8 nil)) (vec pawn-dark) backline-dark]))
 
-(defn init-b []
-  (for [lightPawns (repeatedly (fn [] (create-figure :pawn :light)))
-        darkPawns (repeatedly (fn [] (create-figure :pawn :dark)))]
-    (take 8 darkPawns)))
+
 
 
 (def state (r/atom {:board [(init-board)] ; Letzte Brett in Board ist das aktuelle
@@ -88,47 +89,115 @@
                     :lightKing nil
                     :darkKing nil
                     :selected nil})) ; ausgewaehlte Figur
+;; zeiger auf das board
+(def board-sate (r/cursor state [:board]))
+;; muss derefed werden, quasi cursor mit einer Funktion
+(def last-board (reagent.ratom/make-reaction #(peek (:board @state))))
+
+
+
 ;; Selektoren
-(defn current-board [state]
-  "liefert das aktuelle Board"
-  (peek (:board @state)))
+;; (defn current-board [state]
+;;   "liefert das aktuelle Board"
+;;   (peek (:board @state)))
+
+(defn nth-in [board y x]
+  (nth (nth board y) x))
+
+(nth-in @last-board 6 1)
+
+;; Fig movement
+(defn square-empty? [board y x]
+  "prueft ob square leer ist, wenn nicht gibt es die Farbe der Figur zurueck"
+  (let [square (nth-in board y x)]
+    (if (= square nil)
+      true
+      (:team square))))
+
+;; (square-empty? @last-board 6 1)
+
+
+(defn square-moveable? [figure board y x]
+  "prueft ob die Figur auf ein Square bewegt werden kann"
+  (let [moveable (square-empty? board y x)]
+    (if (= moveable true)
+      true
+      (not (= (:team figure) moveable)))))
+
+;; (square-moveable? (nth-in @last-board 6 0) @last-board 7 1)
+
+(defn probe-while
+  [figure board current-y current-x y-direction x-direction]
+  "prueft ob die Figur sich in eine Richtung bewegen werden kann"
+  (loop [board (vec (repeat 8 (vec (repeat 8 false))))
+         cur-y current-y
+         cur-x current-x
+         moveable (square-moveable? figure board (+ current-y y-direction) (+ current-x x-direction))]
+    (if moveable
+      (let [new-board (assoc board cur-y (assoc (nth board cur-y) cur-x true))] ; setzt board an der richtigen stelle auf true
+        (recur new-board (+ cur-y y-direction) (+ cur-x x-direction) (square-moveable? figure board (+ cur-y y-direction) (+ cur-x x-direction))))
+      board)))
+
+;; (def b (vec (repeat 8 (vec (repeat 8 false)))))
+;; (assoc b 0 (assoc (nth b 0) 1 true))
+;; (probe-while (nth-in @last-board 6 0) @last-board 6 0 -1 0)
+
+;; (defn combine-probe-while [figure board current-y current-x y-dir x-dir]
+;;   "prueft via probe-while mit allen Kombinationen aus dem array y-dir und x-dir"
+;;   (reduce
+;;    (fn [col previous] (map))
+;;    (for [y y-dir]
+;;         x x-dir
+;;     (probe-while figure board current-y current-x y x))))
+
 
 ;;
 
-(defn figure [type team])
+(defn figure [figure]
+  [:img (style/use-style css-figure {:src (:img figure)})])
+
+
+(defn square [y x fig]
+  "Eine Komponente zum darstellen eines Quadrats"
+  (let [tf (iterate (fn [x] (not x)) true)]
+    [:div.Square
+      (style/use-style
+                  (into css-square
+                      (if (nth tf (+ (* y 8) x y))
+                        {:background-color "gray"}
+                        {:background-color "#222222"}))
+                  {:on-click #(.log js/console "hey")})
+     (when fig
+      [figure fig])]))
 
 (defn map-board [board]
   "Gibt eine Sequenz valider Hiccup Syntax in From <row> 8 x <square /></row>"
-  (let [tf (iterate (fn [x] (not x)) true)]
-   (map-indexed
-    (fn [rIndex row]
-      (into [:div.Row (style/use-style css-row)]
-            (map-indexed
-             (fn [cIndex cell]
-               [:div.Square (style/use-style (into css-square
-                                                 (if (nth tf (+ (* rIndex 8) cIndex rIndex))
-                                                   {:background-color "gray"}
-                                                   {:background-color "#222222"})))])
-             row)))
-    board)))
-
+  (map-indexed
+   (fn [rIndex row]
+     (into [:div.Row (style/use-style css-row)]
+           (map-indexed
+            (fn [cIndex cell]
+              [square rIndex cIndex cell])
+            row)))
+   board))
 
 
 (defn board [board]
   "Valide Hiccup Syntax zum darstellen des 8x8 Brett via divs"
   (into
-    [:div.Board (style/use-style css-board)]
-    (map-board board)))
+   [:div.Board (style/use-style css-board)]
+   (map-board board)))
 
 (defn game [state]
   [:div.Game (style/use-style css-game)
-   (board (current-board state))])
+   [board @last-board]])
 
 (defn init-app []
   (style/class "*" {:box-sizing "border-box"})
-  (r/render (game state) (.getElementById js/document "root")))
+  (r/render [game state] (.getElementById js/document "root")))
 
 (defn ^:export main! []
   (println "App loaded!")
   (style/init)
   (init-app))
+
